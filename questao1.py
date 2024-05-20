b6e8f13ec2790da0a65d054541215d9547f13924
Antonio Junior <111703609+antonio-junior34@users.noreply.github.com>
2024-05-20 10:32:15 -0300

    Create questao1.py

---
+++ questao1.py
@@ -0,0 +1,22 @@
+'''
+Escreva um código onde o usuário vai digitar o lado1 e o lado2 do retângulo e imprima este retângulo
+em formato de asteriscos
+Exemplo1: lado1=2 e lado2=4
+* * * *
+* * * *
+Exemplo2: lado1=3 e lado2 = 10
+* * * * * * * * * *
+* * * * * * * * * *
+* * * * * * * * * *
+'''
+
+l = int(input("Digite o lado1 da matriz: "))
+c = int(input("Digite o lado2 da matriz: "))
+
+matriz = []
+
+for i in range(l):
+    matriz.append('*' * c)
+    
+for linha in matriz:
+    print(linha)
